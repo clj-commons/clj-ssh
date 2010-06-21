08b3e8ade2cacfd9ddaac209c08e941f21ca300e
Hugo Duncan <hugo_duncan@yahoo.com>
2010-06-21 09:55:56 -0400

    added autodoc config. updated swank-clojure dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [com.jcraft/jsch "0.1.42"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
-                     [autodoc "0.7.0"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [autodoc "0.7.0"]
+                     [log4j/log4j "1.2.14"]]
+  :autodoc {:name "clj-ssh"
+	    :description "Library for using SSH from clojure."
+	    :copyright "Copyright Hugo Duncan 2010. All rights reserved."
+	    :web-src-dir "http://github.com/hugoduncan/clj-ssh/blob/"
+	    :web-home "http://hugoduncan.github.com/clj-ssh/" })
