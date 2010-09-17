f1fc3d2e08ff299bf7568f9b6cd19b1af11cee44
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-17 06:41:59 -0400

    formatting

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                      [autodoc "0.7.0"]
                      [log4j/log4j "1.2.14"]]
   :autodoc {:name "clj-ssh"
-	    :description "Library for using SSH from clojure."
-	    :copyright "Copyright Hugo Duncan 2010. All rights reserved."
-	    :web-src-dir "http://github.com/hugoduncan/clj-ssh/blob/"
-	    :web-home "http://hugoduncan.github.com/clj-ssh/" })
+            :description "Library for using SSH from clojure."
+            :copyright "Copyright Hugo Duncan 2010. All rights reserved."
+            :web-src-dir "http://github.com/hugoduncan/clj-ssh/blob/"
+            :web-home "http://hugoduncan.github.com/clj-ssh/" })
