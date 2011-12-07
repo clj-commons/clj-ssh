dcd5791e264eca2aaf60094b25fa8b3f1d6d0336
Hugo Duncan <hugo@hugoduncan.org>
2011-12-07 15:06:47 -0500

    Update to 0.3.1-SNAPSHOT. Remove autodoc and swank-clj from dev-dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject clj-ssh "0.3.0"
+(defproject clj-ssh "0.3.1-SNAPSHOT"
   :description "ssh from clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/tools.logging "0.1.2"]
                  [slingshot "0.2.0"]
                  [com.jcraft/jsch "0.1.44-1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [autodoc "0.7.1"]
-                     [log4j/log4j "1.2.14"]]
+  :dev-dependencies [[log4j/log4j "1.2.14"]]
   :autodoc {:name "clj-ssh"
             :description "Library for using SSH from clojure."
             :copyright "Copyright Hugo Duncan 2010, 2011. All rights reserved."
             :web-src-dir "http://github.com/hugoduncan/clj-ssh/blob/"
-            :web-home "http://hugoduncan.github.com/clj-ssh/" })
+            :web-home "http://hugoduncan.github.com/clj-ssh/"})
