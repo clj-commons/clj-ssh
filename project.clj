d52eb67c6b4acff62165c5871f30d7d9c51aa94a
David Barker <kzar@kzar.co.uk>
2011-12-07 16:46:44 +0000

    Switched to Autodoc 0.9.0 to avoid clojure-contrib 1.1.0-master-SNAPSHOT dependency problem.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
                  [slingshot "0.2.0"]
                  [com.jcraft/jsch "0.1.44-1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [autodoc "0.7.1"]
+                     [org.clojars.weavejester/autodoc "0.9.0"]
                      [log4j/log4j "1.2.14"]]
   :autodoc {:name "clj-ssh"
             :description "Library for using SSH from clojure."
             :copyright "Copyright Hugo Duncan 2010, 2011. All rights reserved."
             :web-src-dir "http://github.com/hugoduncan/clj-ssh/blob/"
-            :web-home "http://hugoduncan.github.com/clj-ssh/" })
+            :web-home "http://hugoduncan.github.com/clj-ssh/"})
