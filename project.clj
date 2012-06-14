be326500cff55dcfc01c004b1708e23bc426578a
Hugo Duncan <hugo@hugoduncan.org>
2012-06-14 17:24:02 -0400

    Add .lein files to .gitignore

--- project.clj
+++ project.clj
@@ -11,16 +11,16 @@
                      [ch.qos.logback/logback-classic "1.0.0"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-api "1.6.1"]
                                   [ch.qos.logback/logback-core "1.0.0"]
-                                  [ch.qos.logback/logback-classic "1.0.0"]]}}
-  ;; :multi-deps {"slingshot-0.10.1" [[slingshot "0.10.1"]
-  ;;                                  [org.clojure/clojure "1.2.1"]]
-  ;;              "clojure-1.2.1" [[slingshot "0.10.1"]
-  ;;                               [org.clojure/clojure "1.2.1"]]
-  ;;              "clojure-1.3.0" [[slingshot "0.10.1"]
-  ;;                               [org.clojure/clojure "1.3.0"]]
-  ;;              "clojure-1.4.0" [[slingshot "0.10.1"]
-  ;;                               [org.clojure/clojure "1.4.0-beta1"]]}
-  ;; :codox {:writer codox-md.writer/write-docs
-  ;;         :version "0.3"
-  ;;         :output-dir "doc/api/0.3"}
-  )
+                                  [ch.qos.logback/logback-classic "1.0.0"]
+                                  [codox-md "0.1.0"]]}}
+  :multi-deps {"slingshot-0.10.1" [[slingshot "0.10.1"]
+                                   [org.clojure/clojure "1.2.1"]]
+               "clojure-1.2.1" [[slingshot "0.10.1"]
+                                [org.clojure/clojure "1.2.1"]]
+               "clojure-1.3.0" [[slingshot "0.10.1"]
+                                [org.clojure/clojure "1.3.0"]]
+               "clojure-1.4.0" [[slingshot "0.10.1"]
+                                [org.clojure/clojure "1.4.0-beta1"]]}
+  :codox {:writer codox-md.writer/write-docs
+          :version "0.4"
+          :output-dir "doc/api/0.4"})
