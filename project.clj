946b7327703389031091f6ea41ce60ad94acb9a4
Hugo Duncan <hugo@hugoduncan.org>
2012-10-12 22:32:07 -0400

    Move to a single release script and use profiles.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clj-ssh "0.4.1-SNAPSHOT"
   :description "Library for using SSH from clojure."
   :url "https://github.com/hugoduncan/clj-ssh"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.1.2"
                   :exclusions [org.clojure/clojure]]
@@ -9,23 +11,4 @@
                   :exclusions [com.jcraft/jsch-agent-proxy]]
                  [slingshot "0.10.2"
                   :exclusions [org.clojure/clojure]]
-                 [com.jcraft/jsch "0.1.48"]]
-  :dev-dependencies [[ch.qos.logback/logback-classic "1.0.0"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.0"]
-                                  [codox-md "0.1.0"
-                                   :exclusions [org.clojure/clojure]]]}}
-  :multi-deps {"slingshot-0.10.1" [[slingshot "0.10.1"]
-                                   [org.clojure/clojure "1.2.1"]]
-               "clojure-1.2.1" [[slingshot "0.10.1"]
-                                [org.clojure/clojure "1.2.1"]]
-               "clojure-1.3.0" [[slingshot "0.10.1"]
-                                [org.clojure/clojure "1.3.0"]]
-               "clojure-1.4.0" [[slingshot "0.10.1"]
-                                [org.clojure/clojure "1.4.0-beta1"]]}
-  :codox {:writer codox-md.writer/write-docs
-          :version "0.4"
-          :output-dir "doc/api/0.4"
-          :exclude [clj-ssh.agent clj-ssh.reflect clj-ssh.keychain]}
-  :marginalia {:dir "doc/api/0.4"}
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+                 [com.jcraft/jsch "0.1.48"]])
