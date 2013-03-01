5aff6422433e4421221a9073ea49bfaf16cb2468
Hugo Duncan <hugo@hugoduncan.org>
2013-03-01 17:48:26 -0500

    Update to official jsch.agentproxy relase jars

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.1.2"
                   :exclusions [org.clojure/clojure]]
-                 [jsch-agent-proxy "0.0.4"]
-                 [jsch-agent-proxy/jsch-agent-proxy-jna "0.0.4"
-                  :exclusions [com.jcraft/jsch-agent-proxy]]
+                 [com.jcraft/jsch.agentproxy.usocket-jna "0.0.5"]
+                 [com.jcraft/jsch.agentproxy.usocket-nc "0.0.5"]
+                 [com.jcraft/jsch.agentproxy.sshagent "0.0.5"]
+                 [com.jcraft/jsch.agentproxy.pageant "0.0.5"]
+                 [com.jcraft/jsch.agentproxy.core "0.0.5"]
+                 [com.jcraft/jsch.agentproxy.jsch "0.0.5"]
                  [com.jcraft/jsch "0.1.49"]])
