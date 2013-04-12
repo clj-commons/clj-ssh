cd8648b06a21391a107959c0f9e522356e23b61f
Hugo Duncan <hugo@hugoduncan.org>
2013-04-12 19:53:43 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ssh "0.5.5"
+(defproject clj-ssh "0.5.6-SNAPSHOT"
   :description "Library for using SSH from clojure."
   :url "https://github.com/hugoduncan/clj-ssh"
   :license {:name "Eclipse Public License"
