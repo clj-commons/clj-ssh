8bbaa21494468441da64e7a8d13e59dee816635c
Erik Assum <erik@assum.net>
2019-07-27 20:42:45 +0100

    Reflect change to clj-commons

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (def agentproxy-version "0.0.9")
 
-(defproject clj-ssh "0.5.14"
+(defproject clj-commons/clj-ssh "0.5.15-SNAPSHOT"
   :description "Library for using SSH from clojure."
-  :url "https://github.com/hugoduncan/clj-ssh"
+  :url "https://github.com/clj-commons/clj-ssh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.logging "0.2.6"
@@ -15,4 +15,4 @@
                  [com.jcraft/jsch.agentproxy.jsch ~agentproxy-version]
                  [com.jcraft/jsch "0.1.53"]]
   :jvm-opts ["-Djava.awt.headless=true"]
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
