424354c754777325616831b6c5c4e3023048e689
Erik Assum <erik@assum.net>
2019-07-27 20:46:40 +0100

    Build on Circle

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,5 @@
 {:dev
  {:dependencies [[ch.qos.logback/logback-classic "1.0.0"]]
-  :plugins [[lein-pallet-release "RELEASE"]]
   :aliases {"test" ["with-profile"
                     "clojure-1.4.0:clojure-1.5.1:clojure-1.6.0:clojure-1.7.0:clojure-1.8.0"
                     "test"]}}
