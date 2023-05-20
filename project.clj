514eefcb83079b326075addf6617daa3e1344976
Agorgianitis Loukas <agorg_louk@icloud.com>
2023-05-20 14:21:34 +0300

    Replace jcraft version to maintained mwiede/jsch fork

--- project.clj
+++ project.clj
@@ -7,12 +7,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.logging "1.2.4"
                   :exclusions [org.clojure/clojure]]
-                 [com.jcraft/jsch.agentproxy.usocket-jna ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.usocket-nc ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.sshagent ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.pageant ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.core ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.jsch ~agentproxy-version]
-                 [com.jcraft/jsch "0.1.55"]]
+                 [com.github.mwiede/jsch "0.2.8"]]
   :jvm-opts ["-Djava.awt.headless=true"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
