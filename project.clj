50a08f7c9b32eb81d9229879677c0ab4b37a0082
Tiago Alves <tiago.alves@datastax.com>
2023-07-25 14:38:10 +0100

    Updates clj-ssh to latest JSch 0.2.9
    Updates agent code for package and class name changes done in
    com.github.mwiede's JSch fork.
    Removes unncessary agentproxy dependencies that have been merged
    in that project and adds more modern jna and junixsocket libs.
    Bumps version to make it explicit this newer dependency.

--- project.clj
+++ project.clj
@@ -1,18 +1,13 @@
-(def agentproxy-version "0.0.9")
-
-(defproject clj-commons/clj-ssh "0.5.16-SNAPSHOT"
+(defproject clj-commons/clj-ssh "0.6.0-SNAPSHOT"
   :description "Library for using SSH from clojure."
   :url "https://github.com/clj-commons/clj-ssh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.logging "1.2.4"
                   :exclusions [org.clojure/clojure]]
-                 [com.jcraft/jsch.agentproxy.usocket-jna ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.usocket-nc ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.sshagent ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.pageant ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.core ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.jsch ~agentproxy-version]
-                 [com.jcraft/jsch "0.1.55"]]
+                 [com.github.mwiede/jsch "0.2.9"]
+                 [net.java.dev.jna/jna "5.13.0"]
+                 [com.kohlschutter.junixsocket/junixsocket-core "2.6.2" :extension "pom"]]
   :jvm-opts ["-Djava.awt.headless=true"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
+
